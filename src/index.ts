4f07e61d9d8da575daf7eb59b968acd5ef52b760
Andrew Noblet <andrewbnoblet@gmail.com>
2021-01-04 10:21:23 -0500

    chore(package): save

--- src/index.ts
+++ src/index.ts
@@ -13,23 +13,4 @@ import "./components/shell-component";
     document.body.appendChild(el);
 })();
 
-const serviceWorker = new ServiceWorker({ source: "/service-worker.js" });
-
-if (location.hostname !== "localhost") {
-    let updateRequested = false;
-    registerServiceWorker({
-        installed: (event) => {
-            if (event.isUpdate) {
-                if (!updateRequested) showUpdateSnackbar();
-                updateRequested = true;
-            }
-        },
-        message: (event) => {
-            if (event.data.meta === "workbox-broadcast-update") {
-                if (!updateRequested) showUpdateSnackbar();
-                updateRequested = true;
-            }
-        },
-        source: "/service-worker.js",
-    });
-}
+new ServiceWorker({ source: "/service-worker.js" });
